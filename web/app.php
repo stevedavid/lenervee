9c04a1c3a1d1feab239fabd218f56582f76dc0d4
stevedavid <steve@steve-david.com>
2016-05-25 00:13:21 +0200

    added umask in console and app.php

--- web/app.php
+++ web/app.php
@@ -17,6 +17,7 @@ $apcLoader = new Symfony\Component\ClassLoader\ApcClassLoader(sha1(__FILE__), $l
 $loader->unregister();
 $apcLoader->register(true);
 */
+umask(0000);
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
