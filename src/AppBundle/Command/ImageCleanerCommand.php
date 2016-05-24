fb341e86f7c0f9b9acb403f8a5b443d0cac53902
stevedavid <steve@steve-david.com>
2016-05-24 22:36:39 +0200

    back/added image cleaner console command

---
+++ src/AppBundle/Command/ImageCleanerCommand.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace AppBundle\Command;
+
+use AppBundle\Entity\Courrier;
+use AppBundle\Entity\Image;
+use AppBundle\Entity\Presse;
+use AppBundle\Entity\Reaction;
+use AppBundle\Entity\Tag;
+use Doctrine\ORM\EntityManager;
+use Proxies\__CG__\AppBundle\Entity\Categorie;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class ImageCleanerCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('nrv:clean:images')
+            ->setDescription('Clean non used images')
+        ;
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $imagesDirectory = __DIR__ . '/../../../web';
+
+        $em = $this
+            ->getContainer()
+            ->get('doctrine')
+            ->getManager()
+        ;
+
+        $courriers = $em
+            ->getRepository('AppBundle:Courrier')
+            ->findAll();
+
+        $images = [];
+        foreach ($courriers as $courrier) {
+            $images[] = $imagesDirectory . '/' . $courrier->getImage()->getPath();
+        }
+        $scannedDirectory = glob($imagesDirectory . '/' . Image::UPLOAD_DIR . '/*.*');
+
+        foreach ($scannedDirectory as $filePath) {
+            if (!in_array($filePath, $images)) {
+                dump('>> Unlinking following file: "' . $filePath . '"');
+                unlink($filePath);
+            }
+        }
+    }
+}
