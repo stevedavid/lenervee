8e7f97a3c2c277b85e5765b5257261ccb3033376
Steve David <steve@steve-david.com>
2016-05-31 15:42:34 +0200

    back/corrected error in FormType image

--- src/AppBundle/Form/Admin/ImageType.php
+++ src/AppBundle/Form/Admin/ImageType.php
@@ -6,13 +6,14 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class ImageType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('path', 'text', [
+            ->add('path', TextType::class, [
                 'attr' => [
                     'readonly' => 'readonly',
                     'class' => 'form-control text-center',
